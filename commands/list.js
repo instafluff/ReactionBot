69f969ace9ed27bffede66e29e8cebf27132dad0
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2020-12-25 22:27:00 +0100

    Fixed r!list

--- commands/list.js
+++ commands/list.js
@@ -1,26 +1,27 @@
 module.exports = (extra) => ({
   description: 'Lists all listeners in this server',
   helpMsg: `Send ${extra.prefix}${extra.commandName} to get all the listeners`,
-  fn: async (_, msg) => msg.channel.send({
-    embed: {
-      title: 'List of all the listeners',
-      description: Object.entries(extra.dbs.db.value[msg.guild.id])
-        .map(([i, v]) => (`__**Channel: ${msg.guild.channels.resolve(i).toString()}**__\n${
-          Object.entries(v).map(([i1, v1]) => (`Message: [[See message]](https://discordapp.com/channels/${msg.guild.id}/${i}/${i1})\n${
-            Object.entries(v1).map(([i2, v2]) => (`${i2} => ${
-              msg.guild.roles.cache.find((v3) => v3.id === v2).toString()
-            }`)).join('\n')
-          }`)).join('\n\n')
-        }`)).join('\n\n'),
-      color: extra.utils.hslToDec(Math.floor(Math.random() * 360), 82, 60),
-      timestamp: Date.now(),
-      footer: {
-        text: 'ReactionRole - by Informa',
+  fn: async (_, msg) => {
+    extra.dbs.db.value = extra.utils.lookFullyForEmpty(extra.dbs.db.value);
+    const description = Object.entries(extra.dbs.db.value[msg.guild.id] ?? {})
+      .map(([i, v]) => (`__**Channel: ${msg.guild.channels.resolve(i).toString()}**__\n${
+        Object.entries(v).map(([i1, v1]) => (`Message: [[See message]](https://discordapp.com/channels/${msg.guild.id}/${i}/${i1})\n${
+          Object.entries(v1).map(([i2, v2]) => (`${i2} => ${
+            msg.guild.roles.cache.find((v3) => v3.id === v2).toString()
+          }`)).join('\n')
+        }`)).join('\n\n')
+      }`)).join('\n\n');
+    msg.channel.send({
+      embed: {
+        title: 'List of all the listeners',
+        description: description === '' ? `No listeners found.\n\n\`${extra.prefix}help listen\` to get started.` : description,
+        color: extra.utils.hslToDec(Math.floor(Math.random() * 360), 82, 60),
+        timestamp: Date.now(),
+        footer: {
+          text: 'ReactionRole - by Informa',
+        },
       },
-      /* fields: extra.utils.debug.logThenReturn(
-        ,
-      ), */
-    },
-  }),
+    });
+  },
   permissions: ['MANAGE_ROLES'],
 });
